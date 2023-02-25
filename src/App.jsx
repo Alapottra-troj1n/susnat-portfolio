2baf130d8c4bef36c2250eceebccab33edfaf333
Alapottra <alapottratrojon@gmail.com>
2023-02-25 22:00:46 +0600

    cursor

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,7 @@ import routes from "./routes/routes";
 import { useDispatch } from "react-redux";
 import { useEffect } from "react";
 import { setLoading } from "./features/appSlice/appSlice";
+import AnimatedCursor from "react-animated-cursor";
 
 function App() {
   const dispatch = useDispatch();
@@ -14,6 +15,22 @@ function App() {
 
   return (
     <div>
+      <AnimatedCursor
+        innerSize={5}
+        outerSize={35}
+        innerScale={1}
+        outerScale={1.7}
+        trailingSpeed={5}
+        clickables={["li", "a", ".click", "button", "img", ".cursor-hover"]}
+        outerStyle={{
+          borderWidth: "1px",
+          borderColor: "black",
+          backgroundColor: "var(--cursor-bg-color)",
+        }}
+        innerStyle={{
+          backgroundColor: "var(--cursor-color)",
+        }}
+      />
           <RouterProvider router={routes} />
     </div>
   );
