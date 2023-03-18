81402ac575d9f43afc7faeb8c0cc50656c54670f
Alapottra <alapottratrojon@gmail.com>
2023-03-18 15:51:01 +0600

    single page done

--- src/pages/StoryPage.jsx
+++ src/pages/StoryPage.jsx
@@ -67,44 +67,31 @@ const StoryPage = () => {
       </div>
       <div className="py-24 lg:px-96 px-10">
         <p className="text-gray-400 font-normal leading-8 font-primary">
-          Rare is that Indian Wedding celebrated with intimacy, for intimacy.
-          Rare is the atypical aesthetics used to decorate an age old tradition.
-          Rare is the coming together of two souls in love amidst large flowers
-          and dusky skies. Rare; thats the word that best describes our bride-
-          Meghna. An ardent believer that its Summer somewhere, our bride
-          married the love of her life in winter but with a sunny twist.
-          Celebrating the Indian traditions with close loved ones and friends
-          who are family, Meghna is close to our hearts for her serene smile and
-          simplistic demeanour. It was a lid back affair, with her best of
-          friends being her sisters, helping her tie the knot with cheer and
-          chime. Who said Love is blind..? All they had to do was see around New
-          York to fall in love ; To Meghna and Karan, may the sun be bright, may
-          the champagne be bubbly, and may it be summer always… Simple.
-          Intimate. Easy.
+         {story.description}
         </p>
 
         <div className="grid grid-cols-1 lg:grid-cols-[60%,40%] mt-20 gap-5">
-          <div className="bg-red-500 h-[35rem] "></div>
-          <div className="bg-red-500 h-[35rem] ]"></div>
+          <div style={{ backgroundImage: `url(${story.photo1})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
+          <div style={{ backgroundImage: `url(${story.photo2})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-[40%,60%] mt-5 gap-5">
-          <div className="bg-red-500 h-[35rem] "></div>
-          <div className="bg-red-500 h-[35rem] ]"></div>
+        <div style={{ backgroundImage: `url(${story.photo3})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
+        <div style={{ backgroundImage: `url(${story.photo4})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-[60%,40%] mt-5 gap-5">
-          <div className="bg-red-500 h-[35rem] "></div>
-          <div className="bg-red-500 h-[35rem] ]"></div>
+        <div style={{ backgroundImage: `url(${story.photo5})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
+        <div style={{ backgroundImage: `url(${story.photo6})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
         </div>
         <div className="grid grid-cols-1 lg:grid-cols-[40%,60%] mt-5 gap-5">
-          <div className="bg-red-500 h-[35rem] "></div>
-          <div className="bg-red-500 h-[35rem] ]"></div>
+        <div style={{ backgroundImage: `url(${story.photo7})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
+        <div style={{ backgroundImage: `url(${story.photo8})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
         </div>
 
         <div className="grid grid-cols-1 lg:grid-cols-[60%,40%] mt-5 gap-5">
-          <div className="bg-red-500 h-[35rem] "></div>
-          <div className="bg-red-500 h-[35rem] ]"></div>
+        <div style={{ backgroundImage: `url(${story.photo9})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
+        <div style={{ backgroundImage: `url(${story.photo10})` }} className="bg-cover bg-center bg-no-repeat h-[35rem] "></div>
         </div>
       </div>
     </div>
