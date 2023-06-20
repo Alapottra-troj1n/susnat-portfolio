159214980744107e9dd03d53a12d9c2bf44d3ef9
alapottra-troj1n <alapottratrojon@gmail.com>
2023-06-20 21:09:41 +0600

    fixed spacing issues

--- src/components/reusables/CallToAction.jsx
+++ src/components/reusables/CallToAction.jsx
@@ -3,41 +3,41 @@ import { FiCalendar, FiMail, FiPhoneCall } from "react-icons/fi";
 
 const CallToAction = ({ scrolltoAppointment }) => {
   return (
-    <div id="calltoaction" className="flex justify-center mt-7 md:mt-14 xl:mt-24">
+    <div id="calltoaction" className="flex justify-center mt-5 md:mt-14 xl:mt-24">
       <div  id="callme"
-        className={`text-gray-700 flex flex-wrap justify-center items-center text-sm md:text-base lg:text-base xl:text-2xl  gap-10 p-3 px-7`}
+        className={`text-gray-700 grid grid-cols-3 text-sm md:text-base lg:text-base xl:text-2xl  gap-5 p-3 px-7`}
         style={{ transition: "all 0.5s ease-in-out" }}
       >
         <a
-          className=" rounded-xl p-5 lg:p-12 flex flex-col items-center gap-10 px-24"
+          className=" rounded-xl p-5 lg:p-12 flex flex-col items-center gap-5 lg:gap-10 lg:px-24"
           href="tel:+61415865704"
          
         >
-          <button className="font-primary  border-2 py-4 px-7 whitespace-nowrap border-gray-700 rounded-full font-semibold hover:bg-gray-700 hover:text-white transition-all">
+          <button className="font-primary lg:py-4 lg:px-7 px-3 py-2 whitespace-nowrap border-gray-700 font-semibold hover:bg-gray-700 hover:text-white transition-all">
             Let's have a call
           </button>
      
-          <FiPhoneCall className="text-primary transition-all text-[5rem] lg:text-[10rem]  " />
+          <FiPhoneCall className="text-primary transition-all text-4xl lg:text-[10rem]  " />
         </a>
         <a
-          className="rounded-xl p-5 lg:p-12 flex flex-col items-center gap-10 px-24"
+          className="rounded-xl p-5 lg:p-12 flex flex-col items-center gap-5 lg:gap-10 lg:px-24"
           href="mailto:stan.visuals01@gmail.com"
         >
-          <button className="font-primary  border-2 py-4 px-7 whitespace-nowrap border-gray-700 rounded-full font-semibold hover:bg-gray-700 hover:text-white transition-all">
-            Mail me if you are busy
+          <button className="font-primary  lg:py-4 lg:px-7 px-3 py-2 whitespace-nowrap border-gray-700  font-semibold hover:bg-gray-700 hover:text-white transition-all">
+           Mail Me
           </button>
        
-          <FiMail className="text-primary transition-all text-[5rem] lg:text-[10rem]" />
+          <FiMail className="text-primary transition-all text-4xl lg:text-[10rem]  " />
         </a>
         <a
           onClick={scrolltoAppointment}
-          className="rounded-xl p-5 lg:p-12 flex flex-col items-center gap-10 px-24"
+          className="rounded-xl p-5 lg:p-12 flex flex-col items-center gap-5 lg:gap-10 lg:px-24"
         >
-          <button className="font-primary  border-2 py-4 px-7 whitespace-nowrap border-gray-700 rounded-full font-semibold hover:bg-gray-700 hover:text-white transition-all">
+          <button className="font-primary lg:py-4 lg:px-7 px-3 py-2 whitespace-nowrap border-gray-700 font-semibold hover:bg-gray-700 hover:text-white transition-all">
             Book a meeting
           </button>
         
-          <FiCalendar className="text-primary transition-all text-[5rem] lg:text-[10rem] " />
+          <FiCalendar className="text-primary transition-all text-4xl lg:text-[10rem]  " />
         </a>
       </div>
     </div>
