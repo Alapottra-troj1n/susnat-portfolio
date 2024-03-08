464117f8ccfd5d5ec396a0e72e198e2a7544ed6b
unknown <alapottratrojon@gmail.com>
2024-03-08 22:55:17 +0600

    updated packages

--- src/pages/Packages.jsx
+++ src/pages/Packages.jsx
@@ -4,9 +4,10 @@ export default function Packages() {
   return (
     <div className="py-36 px-5 lg:max-w-6xl mx-auto">
       <div className="mt-5 lg:mt-14">
-        <h2 className="text-center text-2xl lg:text-4xl font-display font-bold">
+        <h2 className="text-center text-2xl lg:text-4xl font-display font-bold  ">
           Photography Packages
         </h2>
+        <div className="w-28 h-1 rounded-md mx-auto mt-4 bg-primary "></div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 mt-14">
           <div className="shadow-md overflow-hidden rounded-md">
@@ -21,10 +22,11 @@ export default function Packages() {
               <h3 className="text-xl font-bold font-display text-center">
                 MINI SESSION
               </h3>
+             
               <p className="text-center text-3xl my-2 font-display font-bold">
                 450 AUD
               </p>
-
+              <div className="w-16 h-1 rounded-md mx-auto my-2 bg-primary "></div>
               <div className="text-left font-primary mt-3 text-gray-600 font-medium">
                 <p className="flex gap-2 items-center">  <TiTick className="text-xl text-green-400" />Unlimited Premium Edited Photos</p>
                 <p className="flex gap-2 items-center">  <TiTick className="text-xl text-green-400" />Duration : 2 Hours</p>
@@ -42,6 +44,9 @@ export default function Packages() {
                   {" "}
                    250 AUD for a
                   personalized photobook (50 photos)
+                </p>
+                <p className="text-xs text-center mt-4 text-gray-400">
+                  You can add addons from previous packages{" "}
                 </p>
               </div>
             </div>
@@ -62,6 +67,7 @@ export default function Packages() {
               <p className="text-center text-3xl my-2 font-display font-bold">
                 750 AUD
               </p>
+              <div className="w-16 h-1 rounded-md mx-auto my-2 bg-primary "></div>
 
               <div className="text-left font-primary mt-3 text-gray-600 font-medium">
               <p className="flex gap-2 items-center">  <TiTick className="text-xl text-green-400" />Unlimited Premium Edited Photos</p>
@@ -103,7 +109,7 @@ export default function Packages() {
               <p className="text-center text-3xl my-2 font-display font-bold">
                 1350 AUD
               </p>
-
+              <div className="w-16 h-1 rounded-md mx-auto my-2 bg-primary "></div>
               <div className="text-left font-primary mt-3 text-gray-600 font-medium">
               <p className="flex gap-2 items-center">  <TiTick className="text-xl text-green-400" />Unlimited Premium Edited Photos</p>
                 <p className="flex gap-2 items-center">  <TiTick className="text-xl text-green-400" />Duration 8 Hours</p>
@@ -115,7 +121,7 @@ export default function Packages() {
                 <p className="text-sm mt-2 flex gap-2 items-center">
                   {" "}
                    400 AUD for a
-                  3-5minute cinematic masterpiece
+                  3-5minute cinematic video
                 </p>
                 <p className="text-sm mt-2 flex gap-2 items-center">
                   {" "}
@@ -133,6 +139,7 @@ export default function Packages() {
         <h2 className="text-center text-2xl lg:text-4xl font-display font-bold mt-24">
           Videography Packages
         </h2>
+        <div className="w-28 h-1 rounded-md mx-auto mt-4 bg-primary "></div>
 
         <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-14 mt-14">
           <div className="shadow-md overflow-hidden rounded-md">
@@ -150,9 +157,7 @@ export default function Packages() {
               <p className="text-center text-3xl my-2 font-display font-bold">
                 750 AUD
               </p>
-
-
-
+              <div className="w-16 h-1 rounded-md mx-auto my-2 bg-primary "></div>
               <div className="text-left font-primary mt-3 text-gray-600 font-medium">
               <h3 className="text-lg mb-2 font-semibold">What's Included ?</h3>
                 <p className="flex gap-2 items-center mb-1"> <TiTick className="text-xl text-green-400" /> Mini Teaser (1-2min)</p>
@@ -181,7 +186,7 @@ export default function Packages() {
               <p className="text-center text-3xl my-2 font-display font-bold">
                 1350 AUD
               </p>
-
+              <div className="w-16 h-1 rounded-md mx-auto my-2 bg-primary "></div>
               <div className="text-left font-primary mt-3 text-gray-600 font-medium">
                 <h3 className="text-lg mb-2 font-semibold">What's Included ?</h3>
               <p className="flex gap-2 items-center mb-1"> <TiTick className="text-xl text-green-400" /> Mini Teaser (1-2min)</p>
